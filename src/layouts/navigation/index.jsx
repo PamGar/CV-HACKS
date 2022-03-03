16af1419e37be1041e0a3564201d80c0bfc507b7
Alexis Salcedo <ale6jss@gmail.com>
2022-03-03 18:49:33 -0400

    Navbar Layout added

--- src/layouts/navigation/index.jsx
+++ src/layouts/navigation/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import '../../styles/normalize.css';
 import styled from 'styled-components';
+import '../../styles/normalize.css';
+import Logo from '../../assets/images/logo_white.png';
+import Profile from '../../assets/images/profile.jpg';
 
 const GridBase = styled.div`
   display: grid;
@@ -28,11 +30,18 @@ const Nav = styled.nav`
   position: sticky;
   top: 0;
   width: 100%;
-  background-color: pink;
+  background-color: #00b7b8;
   height: 100vh;
   max-height: 100vh;
   margin: 0;
   z-index: 7;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .grow {
+    flex-grow: 1;
+  }
 
   @media (max-width: 1099px) {
     height: 70px;
@@ -43,10 +52,37 @@ const Nav = styled.nav`
   }
 `;
 
+const LogoContainer = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 20px;
+
+  img {
+    width: 70%;
+  }
+`;
+
+const MenuIcons = styled.div``;
+
+const ProfileBox = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+
+  img {
+    border-radius: 50%;
+    width: 70%;
+  }
+
+  a {
+    font-weight: 700;
+    color: #891010;
+    text-decoration: none;
+  }
+`;
+
 const Main = styled.div`
   width: 100%;
   grid-area: main / main / main / main;
-  background-color: blue;
 
   @media (max-width: 820px) {
     width: 100vw;
@@ -66,7 +102,17 @@ const Tasks = styled.div`
 const index = () => {
   return (
     <GridBase>
-      <Nav>Navigation</Nav>
+      <Nav>
+        <LogoContainer>
+          <img src={Logo} alt="" />
+        </LogoContainer>
+        <MenuIcons>Icons</MenuIcons>
+        <div className="grow"></div>
+        <ProfileBox>
+          <img src={Profile} alt="" />
+          <a href="">Logout</a>
+        </ProfileBox>
+      </Nav>
       <Main>Main</Main>
       <Tasks>Tasks</Tasks>
     </GridBase>
