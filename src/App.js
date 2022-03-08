c2c6382f7242ebf34c5ed6e86897a8be07ecf442
Carlos Botero <boterocev@gmail.com>
2022-03-08 13:24:39 -0300

    Added CV_preview component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import { GlobalStyles } from './styles/GlobalStyles';
 import Login from './pages/Login';
+import CV_preview from './pages/cv_preview';
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
           <Route path='/login/company' element={<Login company />} />
         </Route>
         <Route element={<PrivateRoute isAuth={false} />}>
-          <Route path='/dashboard' element={<div>dashboard page</div>} />
+          <Route path='/dashboard' element={<CV_preview />} />
         </Route>
       </Routes>
     </Router>
