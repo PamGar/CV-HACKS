1046f50c534e557c8aa329e14ea6345ffbe09237
Carlos Botero <boterocev@gmail.com>
2022-03-09 17:46:16 -0300

    Added a slash

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -128,7 +128,7 @@ const Login = ({ company }) => {
 
     try {
       const { data } = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/user`,
+        `${process.env.REACT_APP_BASE_URL}/user/`,
         user
       );
       setInputError((prev) => ({
