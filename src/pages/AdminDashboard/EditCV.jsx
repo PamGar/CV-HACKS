4cf0e6ef60a15134b6fe00ea6aacdc7b0f0fd410
Carlos Botero <boterocev@gmail.com>
2022-03-23 13:20:39 -0300

    Created component for edit and share cv (incomplete)

---
+++ src/pages/AdminDashboard/EditCV.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditCV = ({ setShowMainContent }) => {
+  return <div>EditCV</div>;
+};
+
+export default EditCV;
---
+++ src/pages/AdminDashboard/ShareCV.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShareCV = ({ setShowMainContent }) => {
+  return <div>ShareCV</div>;
+};
+
+export default ShareCV;
