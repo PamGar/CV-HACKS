02cc315fcf027057ca1822cb0b2c9ac42a821ebd
Carlos Botero <boterocev@gmail.com>
2022-03-22 23:29:32 -0300

    Eliminated privateRoutes.

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -4,20 +4,4 @@ const PrivateRoute = ({ isAuth }) => {
   return isAuth ? <Outlet /> : <Navigate to='/' />;
 };
 
-export const PrivateRouteAdmin = ({ role }) => {
-  return +role === 2 ? <Outlet /> : <Navigate to='/' />;
-};
-
-export const PrivateRouteCompany = ({ role }) => {
-  return +role === 3 ? <Outlet /> : <Navigate to='/' />;
-};
-
-export const PrivateRoutePadawan = ({ role }) => {
-  return +role === 4 ? <Outlet /> : <Navigate to='/' />;
-};
-
-export const PrivateRouteExternal = ({ role }) => {
-  return +role === 5 ? <Outlet /> : <Navigate to='/' />;
-};
-
 export default PrivateRoute;
