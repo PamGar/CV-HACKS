74c6951af4da8d3bd038549bbc75bb7c193c38a9
Carlos Botero <boterocev@gmail.com>
2022-03-22 21:14:42 -0300

    display list of comments

--- src/pages/AdminDashboard/AddCommentCV.jsx
+++ src/pages/AdminDashboard/AddCommentCV.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import FormWrapper from '../../components/FormWrapper';
-import AlertMessage from '../../components/AlertMessage';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import EditCommentModal from './EditCommentModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 
 const Textarea = styled.textarea`
+  box-shadow: 0px 3px 5px 0px rgb(0 0 0 / 20%), 0px 2px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 8px 0px rgb(0 0 0 / 12%);
   max-width: 100%;
   width: 100%;
   resize: vertical;
   overflow-wrap: break-word;
   padding: 8px 15px;
+  border-radius: 3px;
 `;
 
 const CommentContainer = styled.div`
@@ -31,7 +37,7 @@ const CommentContainer = styled.div`
 
 const AreaContainer = styled.div`
   display: grid;
-  grid-template-columns: 0.8fr minmax(210px, 1fr);
+  grid-template-columns: 1fr minmax(210px, 1fr);
   align-items: center;
 
   p:nth-child(2) {
@@ -45,79 +51,137 @@ const AreaContainer = styled.div`
 
 const Select = styled.select`
   padding: 8px 15px;
+  box-shadow: 0px 3px 5px 0px rgb(0 0 0 / 20%), 0px 2px 5px 0px rgb(0 0 0 / 14%),
+    0px 1px 8px 0px rgb(0 0 0 / 12%);
+  border-radius: 3px;
+`;
+
+const IconButton = styled.button`
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+  background-color: transparent;
+
+  .editComment {
+    height: 20px;
+    width: 20px;
+  }
 `;
 
 const AddCommentCV = ({ setShowMainContent }) => {
-  const [comment, setComment] = useState('');
-  const [commentList, setCommentList] = useState([
-    {
-      id: 1,
-      comment: 'Recuerda agregar la ultima experiencia laboral',
-      area: 'Experiencia',
-      status: 1,
-    },
-    {
-      id: 2,
-      comment: 'Coloca tus 2 nombres y apellidos',
-      area: 'Información personal',
-      status: 2,
-    },
-  ]);
-  const [area, setArea] = useState('Informacion Personal');
+  const [comment, setComment] = useState({
+    comment: '',
+    description: 'Informacion Personal',
+  });
+  const [commentList, setCommentList] = useState([]);
+  const [openEditCommentModal, setOpenEditCommentModal] = useState(false);
+  const [commentSelectedID, setCommentSelectedID] = useState(0);
   const handleChange = (e) => {
-    setComment(e.target.value);
+    setComment((prev) => ({ ...prev, comment: e.target.value }));
   };
+  const token = localStorage.getItem('authToken');
+
+  const WriteAComment = async () => {
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/cv/admin-cv-comments/1`,
+        comment,
+        { headers: { authorization: `Token ${token}` } }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const getListOfComments = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}/cv/admin-cv-comments/1`,
+        { headers: { authorization: `Token ${token}` } }
+      );
+      setCommentList([...data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getListOfComments();
+  }, []);
   return (
-    <FormWrapper
-      onClick={() => console.log('sending information to API')}
-      setShowMainContent={setShowMainContent}
-      disableButton={!comment}
-    >
-      <h2>Agregar correción</h2>
-      <Textarea
-        rows='3'
-        columns='15'
-        onChange={handleChange}
-        value={comment}
-        autoFocus
-      ></Textarea>
-      <Select onChange={(e) => setArea(e.target.value)}>
-        <option value='Informacion Personal'>Informacion Personal</option>
-        <option value='Estudios'>Estudios</option>
-        <option value='Experiencia'>Experiencia</option>
-        <option value='Cursos'>Cursos</option>
-      </Select>
-      <h3>Preview</h3>
-      <CommentContainer>
-        <AreaContainer>
-          <p>Area:</p>
-          <p>{area}</p>
-        </AreaContainer>
-        <p>Correción:</p>
-        {comment && <p className='comment'>{comment}</p>}
-      </CommentContainer>
-      <h3>Lista de comentarios</h3>
-      {commentList.map((comment) => (
-        <CommentContainer key={comment.id}>
+    <>
+      <FormWrapper
+        onClick={WriteAComment}
+        setShowMainContent={setShowMainContent}
+        disableButton={!comment.comment}
+      >
+        <h2>Agregar correción</h2>
+        <Textarea
+          rows='3'
+          columns='15'
+          onChange={handleChange}
+          value={comment.comment}
+          autoFocus
+        ></Textarea>
+        <Select
+          onChange={(e) =>
+            setComment((prev) => ({ ...prev, description: e.target.value }))
+          }
+        >
+          <option value='Informacion Personal'>Informacion Personal</option>
+          <option value='Estudios'>Estudios</option>
+          <option value='Experiencia'>Experiencia</option>
+          <option value='Cursos'>Cursos</option>
+        </Select>
+        <h3>Preview</h3>
+        <CommentContainer>
           <AreaContainer>
             <p>Area:</p>
-            <p>{comment.area}</p>
+            <p>{comment.description}</p>
           </AreaContainer>
-          <AreaContainer>
-            <p>Status:</p>
-            <AlertMessage
+          <p>Correción:</p>
+          {comment.comment && <p className='comment'>{comment.comment}</p>}
+        </CommentContainer>
+        <h3>Lista de comentarios</h3>
+        {commentList.map(({ comment, id, description }) => (
+          <CommentContainer key={id}>
+            <IconButton
+              onClick={(e) => {
+                e.preventDefault();
+                setOpenEditCommentModal(true);
+                setCommentSelectedID(id);
+              }}
+            >
+              <FontAwesomeIcon icon={faPenToSquare} className='editComment' />
+            </IconButton>
+            <AreaContainer>
+              <p>Area:</p>
+              <p>{description}</p>
+            </AreaContainer>
+            <AreaContainer>
+              <p>Status:</p>
+              {/* <AlertMessage
               fullWidth
               info={comment.status === 1 ? true : false}
               success={comment.status === 1 ? false : true}
             >
               {comment.status === 1 ? 'enviado' : 'terminado'}
-            </AlertMessage>
-          </AreaContainer>
-          <p>Correción:</p>
-          <p className='comment'> {comment.comment}</p>
-        </CommentContainer>
-      ))}
-    </FormWrapper>
+            </AlertMessage> */}
+            </AreaContainer>
+            <p>Correción:</p>
+            <p className='comment'>{comment}</p>
+          </CommentContainer>
+        ))}
+      </FormWrapper>
+      {openEditCommentModal && (
+        <EditCommentModal
+          openEditCommentModal={openEditCommentModal}
+          setOpenEditCommentModal={setOpenEditCommentModal}
+          commentID={commentSelectedID}
+          commentList={commentList}
+          setCommentList={setCommentList}
+        />
+      )}
+    </>
   );
 };
 
