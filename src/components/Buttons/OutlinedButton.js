2fd5734636699f4fc1222fbf03e7a6accb62b09c
Carlos Botero <boterocev@gmail.com>
2022-04-01 15:34:07 -0300

    Added disabled prop.

--- src/components/Buttons/OutlinedButton.js
+++ src/components/Buttons/OutlinedButton.js
@@ -2,19 +2,31 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   background-color: ${(props) => props.bgColor || 'transparent'};
-  border: 1px solid #00b7b8;
-  color: #00b7b8;
+  border: 1px solid;
+  color: ${(props) => (props.disabled ? '#888888' : '#00b7b8')};
+  border-color: ${(props) => (props.disabled ? '#888888' : ' #00b7b8')};
   border-radius: 5px;
-  cursor: pointer;
+  cursor: ${(props) => (props.disabled ? 'auto' : 'pointer')};
   text-transform: uppercase;
   width: ${(props) => (props.fullWidth ? '100%' : 'max-content')};
   padding: 10px 16px;
   font-size: 0.875rem;
 `;
 
-const OutlinedButton = ({ children, onClick, fullWidth, bgColor }) => {
+const OutlinedButton = ({
+  children,
+  onClick,
+  fullWidth,
+  bgColor,
+  disabled,
+}) => {
   return (
-    <Button onClick={onClick} fullWidth={fullWidth} bgColor={bgColor}>
+    <Button
+      onClick={onClick}
+      fullWidth={fullWidth}
+      bgColor={bgColor}
+      disabled={disabled}
+    >
       {children}
     </Button>
   );
