1f76ad9de6ec9eeddae5ddf422e15a6d1fd8fac4
Carlos Botero <boterocev@gmail.com>
2022-04-07 13:12:49 -0300

    React V-18 setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import i18next from './components/i18next';
 import { I18nextProvider } from 'react-i18next';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Suspense fallback={<div>cargando....</div>}>
-      <I18nextProvider i18next={i18next}>
-        <App />
-      </I18nextProvider>
-    </Suspense>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Suspense fallback={<div>cargando....</div>}>
+    <I18nextProvider i18next={i18next}>
+      <App />
+    </I18nextProvider>
+  </Suspense>
 );
