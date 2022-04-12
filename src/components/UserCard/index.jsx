2994030936b3e40652575da21c57ce6c9c100bfc
Carlos Botero <boterocev@gmail.com>
2022-04-12 14:51:06 -0300

    Added some styles and changed box shadow

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -14,8 +14,8 @@ const UserCardContainer = styled.div`
   text-align: center;
   place-items: center;
   color: #171717;
-  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
-    0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+  box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px,
+    rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
   transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
   cursor: pointer;
 
