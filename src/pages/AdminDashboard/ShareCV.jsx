46b4636130250adc3e549ab25417903b09e3efe7
Carlos Botero <boterocev@gmail.com>
2022-04-13 21:02:59 -0300

    Added share CV.

--- src/pages/AdminDashboard/ShareCV.jsx
+++ src/pages/AdminDashboard/ShareCV.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import MainContentWrapper from '../../components/MainContentWrapper';
+import styled from 'styled-components';
+import MultipleEmail from '../../components/MultipleEmail';
 
 const ShareCV = ({ setShowMainContent }) => {
-  return <div>ShareCV</div>;
+  const [emailList, setEmailList] = useState([]);
+  const [loading, setLoading] = useState({ loading: false, disable: false });
+
+  const postEmailList = async () => {
+    setLoading((prev) => ({ loading: true, disable: true }));
+    try {
+      const data = await new Promise((reject, resolve) => {
+        setTimeout(() => resolve(emailList), 2000);
+      });
+      console.log(data);
+      setLoading((prev) => ({ loading: false, disable: false }));
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+      setLoading((prev) => ({ loading: false, disable: false }));
+    }
+  };
+  return (
+    <MainContentWrapper
+      loadingButtonTitle='compartir'
+      onClickOutlinedButton={() => setShowMainContent('CVlist')}
+      onClickLoadingButton={postEmailList}
+      loading={loading.loading}
+      disableButton={loading.disable || !emailList.length > 0}
+    >
+      <h1>Compartir CV</h1>
+      <MultipleEmail emailList={emailList} setEmailList={setEmailList} />
+    </MainContentWrapper>
+  );
 };
 
 export default ShareCV;
