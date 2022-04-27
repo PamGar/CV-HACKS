e9efb9a2377f76699973686d0f5ce0a96cefd73f
Carlos Botero <boterocev@gmail.com>
2022-04-27 15:29:04 -0300

    Created delete job offer modal.

---
+++ src/pages/JobOffersCompany/DeleteJobOfferModal.jsx
@@ -0,0 +1,61 @@
+import { useRef } from 'react';
+import Modal from '../../components/Modal';
+import ModalLayout from '../../components/Modal/ModalLayout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { toast } from 'react-toastify';
+import LoadingButton from '../../components/Buttons/LoadingButton';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const FontAwesomeIconStyled = styled(FontAwesomeIcon)`
+  width: 50px;
+  height: 50px;
+  color: red;
+`;
+
+const DeleteJobOfferConfirmModal = ({
+  openModal,
+  id,
+  setOpenModal,
+  setJobOfferData,
+}) => {
+  const ModalLayoutRef = useRef();
+
+  const DeleteJobOffer = async () => {
+    try {
+      const { data } = await axios.delete(
+        `${process.env.REACT_APP_BASE_URL}/company/vacancy/${id}`,
+        {
+          headers: {
+            authorization: `Token ${localStorage.getItem('authToken')}`,
+          },
+        }
+      );
+      setJobOfferData((prev) => prev.filter((jobOffer) => jobOffer.id !== id));
+      toast.success(data.message);
+      ModalLayoutRef.current.classList.add('fadeOut');
+      setTimeout(() => setOpenModal(false), 250);
+    } catch (err) {
+      console.log(err);
+      toast.error('opps no se pudo eliminar la vacante, intente de nuevo');
+    }
+  };
+  return (
+    <Modal
+      isOpen={openModal}
+      element={
+        <ModalLayout ref={ModalLayoutRef} setOpenModal={setOpenModal}>
+          <FontAwesomeIconStyled icon={faTrashCan} />
+          <h1>¿Estás seguro?</h1>
+          <p>Elminiras la vacante y no podrás recuperarla</p>
+          <LoadingButton onClick={DeleteJobOffer} fullWidth>
+            Confirmar
+          </LoadingButton>
+        </ModalLayout>
+      }
+    />
+  );
+};
+
+export default DeleteJobOfferConfirmModal;
