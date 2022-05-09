fb8192805aa7dd1400685d9e8c9ddbe93da66fb4
Carlos Botero <boterocev@gmail.com>
2022-05-09 20:46:05 -0300

    Added login button for companies

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -5,64 +5,60 @@ import { NavLink } from 'react-router-dom';
 import Coder from '../../assets/images/Recurso1.svg';
 import Logo from '../../assets/images/logo_white.png';
 
-const Landing = () => {
-  const LandingBox = styled.div`
-    height: 100vh;
+const LandingBox = styled.div`
+  height: 100vh;
 
-    button {
-      background-color: #08adb4;
-    }
+  a {
+    text-decoration: none;
+  }
+`;
+const Nav = styled.div`
+  height: 100px;
+  padding: 0 20px;
+  background-color: #a0a0cc;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    a {
-      text-decoration: none;
+  .logo {
+    height: 60%;
+
+    img {
+      height: 100%;
     }
-  `;
-  const Nav = styled.div`
-    height: 100px;
-    padding: 0 20px;
-    background-color: #a0a0cc;
+  }
+`;
+const Body = styled.div`
+  height: calc(100% - 100px);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 20px;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+
+  div {
+    width: 50%;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
     align-items: center;
-
-    .logo {
-      height: 60%;
-
-      img {
-        height: 100%;
-      }
-    }
-  `;
-  const Body = styled.div`
-    height: calc(100% - 100px);
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    padding: 20px;
+    justify-content: center;
+    text-align: center;
 
     @media (max-width: 1000px) {
-      flex-direction: column;
+      width: 100%;
     }
+  }
+`;
 
-    div {
-      width: 50%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      text-align: center;
-
-      @media (max-width: 1000px) {
-        width: 100%;
-      }
-    }
-  `;
-
+const Landing = () => {
   return (
     <LandingBox>
       <Nav>
-        <div className="logo">
-          <img src={Logo} alt="" />
+        <div className='logo'>
+          <img src={Logo} alt='' />
         </div>
         <div>
           <p style={{ color: '#595393', fontWeight: '700', fontSize: '18px' }}>
@@ -84,12 +80,25 @@ const Landing = () => {
             Ingresa a la plataforma y<br />
             comienza ya a crear tu CV
           </h1>
-          <NavLink to="/login" style={{ marginTop: '10px' }}>
+          <NavLink to='/login' style={{ marginTop: '10px' }}>
             <Button>Ingresar</Button>
           </NavLink>
+          <p style={{ marginTop: '10px', fontSize: '14px' }}>
+            Si eres compañia ingresa{' '}
+            <NavLink
+              to='/login/company'
+              style={{
+                textDecoration: 'underline',
+                fontSize: '14px',
+                color: '#6363a8',
+              }}
+            >
+              aqui
+            </NavLink>
+          </p>
         </div>
         <div>
-          <img src={Coder} alt="" />
+          <img src={Coder} alt='' />
         </div>
       </Body>
     </LandingBox>
